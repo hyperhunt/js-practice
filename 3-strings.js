36b6ce260873d97506e54a251c5d54801bc55d4d
Tony Branch <tonybranch.org@yahoo.com>
2020-09-02 21:56:07 +0300

    Add 3-strings.js and methods.

--- 3-strings.js
+++ 3-strings.js
@@ -1,10 +1,63 @@
 // Strings
 
+// const firstName = 'Tony'
+// const lastName = 'Hunt'
+// const age = 42
+// const str = 'Hello, World!'
+
+// let value = str;
+// value = firstName + ' ' + lastName;
+// value += ' ' + age;
+
+// console.log(value);
+
+// value = firstName.length;
+// console.log('firstName: ' + value);
+
+// value = str[str.length - 1];
+// console.log(value);
+
+
+// // Method for Strings
+
+// value = firstName.toLowerCase();
+// value = firstName.toLocaleUpperCase();
+// value = firstName.concat(' ', 'tada!');
+// value = firstName.indexOf('o');
+// value = firstName.indexOf('a'); // -1, not found
+
+// value = str.includes('Hello')
+
+// value = str.slice(0, 5); // Hello
+// value = str.replace('World', 'Tony')
+
+// console.log(value);
+
+
+// Style ES5
 const firstName = 'Tony'
-const lstName = 'Hunt'
+const lastName = 'Hunt'
 const age = 42
-const str = 'Hello, World!'
 
-let value = str;
+let str;
+str = 'Hello my name is ' + firstName + ' ' + lastName;
+console.log(str);
 
-console.log(value);
+
+str = '<ul>'
+    + '<li>First name: ' + firstName + '</li>'
+    + '</ul>'
+// console.log(str);
+
+// Style ES6
+
+str = `
+<ul>
+    <li>First name: ${firstName}</li>
+    <li>Last name: ${lastName}</li>
+    <li>Age: ${age}</li>
+    <li>Math random: ${Math.random()}</li>
+</ul>
+`;
+
+document.body.innerHTML = str;
