10f7fcb8b303e1915cfcf9173fef7efbaf2a7c66
Tony Branch <tonybranch.org@yahoo.com>
2020-09-02 19:03:53 +0300

    Add 1-dataTypes.js.

---
+++ 1-dataTypes.js
@@ -0,0 +1,12 @@
+// ## REVISING STRINGS
+
+// let example = 'this example exists'
+// example = example.replace('exists', 'is awesome')
+// console.log(example);
+
+// let example = 'pizza is alright';
+// example = example.replace('alright', 'wonderful');
+// console.log(example);
+
+// ## NUMBERS
+
