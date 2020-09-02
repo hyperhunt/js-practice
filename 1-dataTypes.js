bcdaba0a36d8552651417598edbbef5c6cd86423
Tony Branch <tonybranch.org@yahoo.com>
2020-09-02 19:58:34 +0300

    Variables: var, let, const.

--- 1-dataTypes.js
+++ 1-dataTypes.js
@@ -55,3 +55,56 @@ console.log({ name: 'Tony', age: 42 });
 console.table({ name: 'Tony', web: 'HyperHunt.dev' });
 
 console.log([1,2,3,4]);
+
+// Variables
+// var 2015
+// EACMScript 6: let, cost
+
+console.clear();
+
+var name = 'Tony'
+console.log(name);
+
+name = 'Hunt'
+console.log(name);
+
+var age;
+console.log(age);
+age = 42;
+console.log(age);
+
+var $name = '$name';
+console.log($name);
+var _name = '_name'
+console.log(_name);
+
+// Style Variables
+var userAge = 42; // camelCase
+var user_age = 42; // mod
+var UserAge = 42; // style for Class
+var userage = 42; // bad variant
+
+console.log(car); // undefined: var car - hosting
+var car = 'bmw'
+var car = 'audi'
+console.log(car);
+
+
+console.clear();
+// let and const
+
+let nickName = 'TonyHunt'
+console.log(nickName);
+
+const firstName = 'Tony'
+console.log(firstName);
+// firstName = 'Hunt' - Uncaught TypeError: invalid assignment to const 'firstName'
+// const a; Uncaught SyntaxError: missing = in const declaration
+
+const user = {
+    name: 'Tony',
+    age: 42
+}
+console.log(user);
+user.name = 'Hunt'
+console.log(user);
