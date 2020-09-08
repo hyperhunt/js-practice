0f4b2371bb8bee12bf4d5fc4c383a721b6759823
Tony Branch <tonybranch.org@yahoo.com>
2020-09-08 14:43:07 +0300

    6-loops: add while, for.

--- 6-loops.js
+++ 6-loops.js
@@ -2,5 +2,25 @@
 
 // while, do while, for, for of, for in
 
-let value;
-console.log(value);
+// while
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// let m = 10;
+// while (i--) {
+//   console.log(i);
+// }
+
+// do {
+//     console.log('Action');
+// } while (i > 0)
+
+// for
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
