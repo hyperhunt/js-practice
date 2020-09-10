93d726d644413a1688f697769dd75632401369d7
Tony Branch <tonybranch.org@yahoo.com>
2020-09-10 09:27:25 +0300

    8-functions.js - Defining functions.

---
+++ 8-functions.js
@@ -0,0 +1,22 @@
+// Defining functions
+
+// Function declarations
+// Function expressions
+
+// function sayHello() {
+//   console.log('Hello World!');
+// }
+
+// sayHello();
+
+// function watchValue(firstName = 'Default', lastName = 'Default') {
+//   return `Hello ${firstName} ${lastName}`;
+// }
+
+// let data = watchValue('Tony', 'Hunt');
+// console.log(data);
+
+// let empty = watchValue();
+// console.log(empty);
+
+//
