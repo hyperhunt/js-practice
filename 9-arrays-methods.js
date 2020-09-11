c797decf631c06609d3ddd977eeeac76c962f20e
Tony Branch <tonybranch.org@yahoo.com>
2020-09-11 12:55:34 +0300

    Array: Basic methods.

---
+++ 9-arrays-methods.js
@@ -0,0 +1,39 @@
+// Arrays methods
+
+const numArr = [1, 32, 3, 4, 5];
+// let value;
+// value = numArr.length;
+
+// value = Array.isArray(numArr); // true
+// value = numArr[2];
+// value = numArr.indexOf(32); // Index 1
+
+// // push - add in end, pop - delete end element
+// value = numArr.push(42);
+// value = numArr.pop();
+
+// // unshift - add in start, shift - delet start element
+// value = numArr.unshift(13);
+// value = numArr.shift();
+
+// // splice - add and/or delete range elements
+// value = numArr.splice(0, 2); // delete 2 elements
+// value = numArr.splice(1, 2, 'one', 'two');
+
+// value = numArr.reverse(); // reverse elements in array
+
+// concat - concatenates the string/array arguments
+// value = numArr.concat(numArr);
+
+// join - creates and returns a new string by concatenating all of the elements in an array
+// joint - array arguments in string
+// value = numArr.join('#');
+
+// split - divides a String into an ordered list of substrings
+// split - return the array
+// value = numArr.toString().split('');
+// console.log('numArr: ' + numArr + ', typeof: ' + typeof numArr);
+// console.log('value: ' + value + ', typeof: ' + typeof value);
+
+// value = 'Hello'.split('')[0]; // return element of index 0 - 'H'
+// console.log(value);
