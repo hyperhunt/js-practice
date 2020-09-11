c3efabf25b671ad3f5c5faa753ca2003d0ce20c0
Tony Branch <tonybranch.org@yahoo.com>
2020-09-11 21:16:14 +0300

    Practical in Callbacks.

---
+++ 10-callbacks.js
@@ -0,0 +1,20 @@
+// Callbacks
+
+// function foo() {
+//   console.log('foo');
+// }
+
+// foo();
+
+// foo.field = 'Tony';
+// console.log(foo.field);
+
+/// Func first class
+const arr = ['Tony', 'Hyper', 'Hunt', 'Dev'];
+// => [4, 5, 4, 3]
+
+console.log(arr);
+
+function mapArray(arr, fn) {
+  const res = [];
+}
