635a421e7397ad63e855bc368c8dfc3d4d3bb664
Tony Branch <tonybranch.org@yahoo.com>
2020-09-21 13:20:43 +0300

    TestCase: Data for Replace literals.

--- str.js
+++ str.js
@@ -1,113 +1,70 @@
-// let data = 'прпвев ярикол';
-// let data = 'ПрИвЕт пРикОл'.toLowerCase();
-let data = 'ааббдд ддббаа';
+let data = 'прпвев ярикол'; // 0
+// let data = 'ПрИвЕт пРикОлл'.toLowerCase(); // 0
+// let data = 'ПрИвЕт пРикОл'.toLowerCase(); // 1
+// let data = 'абаб ааах'; // 0
+// let data = 'ааббдд ддббаа'; // 1
 console.log('Input data: ' + data);
 
+// Получение подстрок.
 let subStrOne = data.slice(0, data.indexOf(' '));
-console.log('#' + subStrOne + '#');
+// console.log('#' + subStrOne + '#');
 let subStrTwo = data.slice(data.indexOf(' ') + 1);
-console.log('#' + subStrTwo + '#');
+// console.log('#' + subStrTwo + '#');
 
-let r = '';
-subStrOne.length == subStrTwo.length ? (r = 1) : (r = 0);
-console.log(r);
+// Функция выражение, в которое производится сравнение срок по длинне, производится замена литералов по паттерну.
+let replaceLetter = function (subStrOne, subStrTwo) {
+  let convertStatus = ''; // Статус конвертации, если длинна не равна - 0, если конвертировать не возможно - 0, иначе 1.
+  let pattern = {}; // Объект для генерации ключей замены литералов
 
-let searchLetter = function (subStrOne, subStrTwo) {
-  let pattern = {};
+  // Проверка подстрок на равенство по количеству элементов
+  subStrOne.length == subStrTwo.length
+    ? (convertStatus = 1)
+    : (convertStatus = 0);
 
-  for (let i = 0; i < subStrOne.length; i++) {
-    console.log(`## ${i} property: ${!pattern.hasOwnProperty(subStrOne[i])}`);
-    if (!pattern.hasOwnProperty(subStrOne[i])) {
-      if (subStrOne[i] != subStrTwo[i]) {
-        if (subStrOne.search(subStrTwo[i]) != -1) {
-          pattern[subStrTwo[i]] = '#'; //  'д' - '#'
-          pattern[subStrOne[i]] = subStrTwo[i]; // 'а' - 'д'
-          pattern['#'] = subStrOne[i]; // '#' - 'а'
+  // Если длинна подстрок равна, тогда выполняем замену литералов
+  if (convertStatus) {
+    for (let i = 0; i < subStrOne.length; i++) {
+      // console.log(`## ${i} property: ${!pattern.hasOwnProperty(subStrOne[i])}`); // Проверка объекта на наличие ключа
+      if (!pattern.hasOwnProperty(subStrOne[i])) {
+        // Проверка на наличие паттерна
+        if (subStrOne[i] != subStrTwo[i]) {
+          // Если символ строки 1 не равен символу строки 2
+          // Если находим литерал строки 2 в строке 1, тогда создаем дополнительную пару в Паттерне для сохранения и змены во всей строке
+          if (!subStrOne.search(subStrTwo[i]) != -1) {
+            pattern[subStrTwo[i]] = '#'; //  'д' - '#'
+            pattern[subStrOne[i]] = subStrTwo[i]; // 'а' - 'д'
+            pattern['#'] = subStrOne[i]; // '#' - 'а'
 
-          subStrOne = subStrOne.split(subStrTwo[i]).join(pattern[subStrTwo[i]]);
-          subStrOne = subStrOne.split(subStrOne[i]).join(pattern[subStrOne[i]]);
-          subStrOne = subStrOne.split('#').join(pattern['#']);
+            // Замена найденного литерала во всей строке 1
+            subStrOne = subStrOne
+              .split(subStrTwo[i])
+              .join(pattern[subStrTwo[i]]);
 
-          // subStrOne = subStrOne
-          //   .split(pattern[subStrOne[i]])
-          //   .join(pattern[subStrTwo[i]]);
-
-          // subStrOne = subStrOne.replace(subStrOne[i], pattern[subStrOne[i]]);
-
-          // subStrOne = subStrOne
-          //   .split(pattern[subStrOne[i]])
-          //   .join(pattern[subStrTwo[i]])
-          //   .replace(subStrOne[i], pattern[subStrOne[i]]);
-
-          //
-          // subStrOne = subStrOne
-          //   .split(pattern[subStrOne[i]])
-          //   .join(pattern[subStrTwo[i]]);
-
-          // subStrOne = subStrOne.split(subStrOne[i]).join(subStrTwo[i]);
-          //
-
-          // subStrOne = subStrOne.split(subStrTwo[i]).join(subStrOne[i]);
-          // pattern[subStrOne[i]] = subStrTwo[i];
-          // subStrOne = subStrOne.replace(
-          //   pattern[subStrOne[i]],
-          //   pattern[subStrTwo[i]],
-          // );
-        } else {
-          // subStrOne = subStrOne
-          //   .split(pattern[subStrOne[i]])
-          //   .join(pattern[subStrTwo[i]]);
-          // subStrOne = subStrOne.split(subStrOne[i]).join(subStrTwo[i]);
-          // console.log(`##${i}: ${subStrOne[i]} ${subStrTwo[i]}`);
-          // subStrOne = subStrOne
-          //   .split(pattern[subStrOne[i]])
-          //   .join(pattern[subStrTwo[i]]);
+            // Замена первой парю ключей
+            subStrOne = subStrOne.replace(subStrOne[i], pattern[subStrOne[i]]);
+          } else {
+            // console.log(`## ${(subStrOne[i], pattern['#'])}`);
+            // Если в строке 1 не было найдено символов из строки 2, осуществляем просто замену литералов
+            subStrOne = subStrOne.replace(subStrOne[i], pattern['#']);
+          }
         }
-
-        // subStrOne.search(subStrTwo[i]) == -1
-        //   ? ((pattern[subStrOne[i]] = subStrTwo[i]),
-        //     (subStrOne = subStrOne.replace(
-        //       subStrOne[i],
-        //       pattern[subStrOne[i]],
-        //     )))
-        //   : (subStrOne = subStrOne.replace(subStrOne[i], '#'));
-        // console.log(`${i} IF: ` + subStrOne);
+      } else {
+        // Замена ключей в случаи, когда литерал строки 1 и строки 2 равны
+        if (subStrOne[i] == subStrTwo[i]) {
+          subStrOne = subStrOne.replace(subStrOne[i], subStrTwo[i]);
+        } else {
+          subStrOne = subStrOne.replace(subStrOne[i], pattern[subStrOne[i]]);
+        }
       }
-
-      // subStrOne = subStrOne.split(subStrOne[i]).join(pattern[subStrOne[i]]);
-      // console.log(`${i} EL: ` + subStrOne);
     }
-    // } else {
-    //   // if (subStrOne.search(pattern[subStrOne[i]]) != -1)
-    //   //   subStrOne = subStrOne.replace(pattern[subStrOne[i]], '#');
-    //   // subStrOne = subStrOne.split(subStrOne[i]).join(pattern[subStrOne[i]]);
-    //   // console.log(`${i} EL: ` + subStrOne);
-    //   // subStrOne = subStrOne.replace(subStrOne[i], pattern[subStrOne[i]]);
-
-    // }
-    console.log(`${i}: ` + subStrOne);
+    console.log(pattern);
+    convertStatus = subStrOne == subStrTwo ? 1 : 0;
+    // console.log(
+    //   `convertStatus: ${subStrOne} ? ${subStrTwo} == ${convertStatus}`,
+    // );
   }
-  console.log(subStrOne);
-  console.log(pattern);
-  // console.log(result);/
 
-  console.log(`R: ${subStrOne} ? ${subStrTwo} == ${subStrOne == subStrTwo}`);
+  return convertStatus;
 };
 
-searchLetter(subStrOne, subStrTwo);
-
-let search = 'д';
-let replaceWith = '#';
-let m = 'ааббдд'.split(search).join(replaceWith);
-console.log(m);
-
-// console.log('## ## ##');
-// let pattern = { пп: 'я', в: 'к', е: 'о' };
-
-// let q = `pattern.hasOwnProperty([subStrOne[i]]): ${pattern.hasOwnProperty(
-//   subStrOne[0],
-// )}`;
-// console.log(q);
-
-// if () {
-//       if (!pattern[subStrOne[i]] && pattern[subStrOne[i]] != subStrTwo[i]) {
+console.log(`Result: ${replaceLetter(subStrOne, subStrTwo)}`);
