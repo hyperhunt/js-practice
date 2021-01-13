1f70db740c51744a616e27eae7d02fcbb28e6f4b
Tony Branch <tonybranch.org@yahoo.com>
2021-01-13 21:36:25 +0300

    DOM: e.target.tagName

--- part3-event-handling-src/app.js
+++ part3-event-handling-src/app.js
@@ -34,24 +34,47 @@ const btn = document.querySelector('button')
 //     console.log(e);
 // })
 
-btn.addEventListener('click', e => {
-    console.log(this)
-    console.log(e);
-})
+// btn.addEventListener('click', e => {
+//     console.log(this)
+//     console.log(e);
+// })
 
-btn.addEventListener('click', function() {
-    console.log(this)
-    console.log('click text 2')
-})
+// btn.addEventListener('click', function() {
+//     console.log(this)
+//     console.log('click text 2')
+// })
 
-function deleteElement(e) {
-    e.preventDefault()
-    console.log(this)
-    console.log('click link');
-}
+// function deleteElement(e) {
+//     e.preventDefault()
+//     console.log(this)
+//     console.log('click link');
+// }
 
-const link = document.querySelector('a')
-// console.log(link.outerHTML);
-link.addEventListener('click', deleteElement)
+// const link = document.querySelector('a')
+// // console.log(link.outerHTML);
+// link.addEventListener('click', deleteElement)
 
 // link.removeEventListener('click', deleteElement)
+
+
+// Dinamic
+const container = document.querySelector('.container')
+console.log(container.outerHTML)
+
+btn.addEventListener('click', e => {
+    const div = document.createElement('div')
+    const nestedBtn = document.createElement('button')
+
+    // div.textContent = (Math.random() * 100).toString().slice(0,2)
+    div.textContent = Math.random()
+    nestedBtn.textContent = 'Button'
+    div.appendChild(nestedBtn)
+    container.appendChild(div)
+})
+
+container.addEventListener('click', e => {
+    // console.log(e.target.tagName)
+    if(e.target.tagName === 'BUTTON') {
+        console.log(e.target)
+    }
+})
