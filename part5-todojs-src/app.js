1b34253aeb153027da4e61d7f6e503074144a8a8
Tony Branch <tonybranch.org@yahoo.com>
2021-03-26 13:49:58 +0300

    Repeate createNewTask(), listItemTemplate()

--- part5-todojs-src/app.js
+++ part5-todojs-src/app.js
@@ -30,3 +30,113 @@ const tasks = [
       'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
   },
 ];
+
+(function(arrOfTasks){
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+  // console.log(objOfTasks);
+
+  // Elements UI
+  const listContainer = document.querySelector(
+    '.tasks-list-section .list-group',
+  );
+
+  const form = document.forms['addTask'];
+  const inputTitle = form.elements['title'];
+  const inputBody = form.elements['body'];
+  console.log(inputTitle, inputBody);
+
+  renderAllTask(objOfTasks);
+  form.addEventListener('submit', onFormSubmitHandler);
+
+  function renderAllTask (taskList) {
+
+    if(!taskList) {
+      console.log("Передайте список задач!");
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+
+    Object.values(taskList).forEach(task => {
+      // console.log(task);
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    listContainer.appendChild(fragment);
+  }
+
+  function listItemTemplate({_id, title, body} = {}) {
+    // console.log(_id, title);
+
+     
+    const li = document.createElement("li");
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'align-items-center',
+      'flex-wrap',
+      'mt-2',
+    );
+    // console.log(li);
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add(
+      'btn',
+      'btn-danger',
+      'ml-auto',
+      'delete-btn',
+    );
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    // console.log(li);
+    return li;
+  }
+  
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    // console.log(titleValue, bodyValue);
+    if(!titleValue || !bodyValue) {
+      alert("Введите Title и Body.")
+    }
+
+    const task = createNewTask(titleValue, bodyValue);
+    // console.log(objOfTasks); 
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement("afterbegin", listItem);
+    form.reset();
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
+    }
+
+    objOfTasks[newTask._id] = newTask;
+    // console.log(objOfTasks);
+    return {...newTask};
+  }
+
+})(tasks);
